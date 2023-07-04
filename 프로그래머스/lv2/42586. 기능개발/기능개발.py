5e4b6b9fa3f12c715136679b13b44daa61f82651
Eddy Lee <71869717+eeeeeddy@users.noreply.github.com>
2023-07-04 23:54:42 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.py
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -10,14 +10,14 @@ def solution(progresses, speeds):
     for i in range(len(progresses)):
         tmp.append(math.ceil((100-progresses[i])/speeds[i]))
 
-    Max = tmp[0]
-    for i in range(1, len(tmp)):
-        if Max >= tmp[i]:
+    Max = tmp[0]                    # 첫 번째 작업을 Max로 지정
+    for i in range(1, len(tmp)):    # 반복문을 수행하면서 Max보다 작은 수를 만나면 cnt 증가 (한 번에 배포 되기 때문)
+        if Max >= tmp[i]:           
             cnt += 1
-        else:
+        else:                       # Max보다 큰 수를 만나면 Max를 변경하고 지금까지의 cnt를 answer에 추가
             Max = tmp[i]
             answer.append(cnt)
             cnt = 1
-    answer.append(cnt)
+    answer.append(cnt)              # 마지막까지 계산된 cnt를 answer에 추가 (추가되기 전에 반복문이 종료되기 때문)
 
     return answer
