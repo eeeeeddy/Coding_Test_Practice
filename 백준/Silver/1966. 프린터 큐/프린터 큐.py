b05c51c25cd687b78ab05da79de6e7dfa582a06d
Eddy Lee <71869717+eeeeeddy@users.noreply.github.com>
2023-07-05 14:38:40 +0900

    Update 프린터 큐.py

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -8,14 +8,13 @@ for i in range(x):
     queue = deque(list(map(int, input().split())))
     order = deque(i for i in range(len(queue)))
     answer = deque()
-    answer_order = deque()
     
     while len(queue) >= 1:
         if queue[0] == max(queue):
-            answer.append(queue.popleft())
-            answer_order.append(order.popleft())
+            queue.popleft()
+            answer.append(order.popleft())
         else:
             queue.append(queue.popleft())
             order.append(order.popleft())
 
-    print(answer_order.index(m) + 1)
+    print(answer.index(m) + 1)
